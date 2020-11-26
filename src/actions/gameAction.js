e026579d556a02aea0cd98c114f6bdc444cec1df
Simona Ene <enesimonam@gmail.com>
2020-11-26 14:00:42 +0200

    5. fetching data

---
+++ src/actions/gameAction.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { popularGamesURL } from "../api";
+
+//action creators
+export const loadGames = () => async (dispatch) => {
+  //fetch axios
+  const popularData = await axios.get(popularGamesURL());
+  dispatch({
+    type: "FETCH_GAMES",
+    payload: {
+      popular: popularData.data.results,
+    },
+  });
+};
---
+++ src/reducers/gamesReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+  popular: [],
+  new: [],
+  upcoming: [],
+  searched: [],
+};
+
+const gamesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_GAMES":
+      return { ...state, popular: action.payload.popular };
+    default:
+      return { ...state };
+  }
+};
+
+export default gamesReducer;
